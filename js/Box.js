04f875aaf3c70707124113b4a161803b94385fb1
Jonathan mehraban <jonathanmehraban@MacBook-Pro-de-Jonathan.local>
2018-05-03 14:10:22 -0500

    first commit

---
+++ js/Box.js
@@ -0,0 +1,40 @@
+function Box(x, y, w, h){
+var options = {
+friction: 0.3,
+restitution:0.6,
+frictionStatic:0
+}
+  this.body = Bodies.rectangle(x, y, w, h, options);
+  this.w = w;
+  this.h = h;
+  World.add(world, this.body);
+
+  this.offScreen = function (){
+	var pos = this.body.position;
+	return (pos.y > height + 100)
+}
+
+this.removeFromWorld = function (){
+	World.remove(world,this.body);
+
+}
+
+
+  this.show = function(){
+    var pos = this.body.position;
+    var angle =this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    noStroke(0);
+    tint(255, 127);
+    fill(255,9,70);
+
+
+
+    rect(0, 0, this.w, this.h);
+    pop();
+ }
+}
---
+++ js/sketch.js
@@ -0,0 +1,220 @@
+//Creamos nu Engine de física (Matter.js)
+var Engine = Matter.Engine,
+    //Render = Matter.Render,
+    //Creamos el mundo
+World = Matter.World,
+    //Creamos los bodies ( array )
+Bodies = Matter.Bodies;
+var engine;
+var box;
+var boxes = [];
+var world;
+var elements = [];
+var factorIncremento = 1;
+var scroll = 0;
+var kickstart = false;
+var isGameOver;
+var meshi;
+var floor;
+var bg;
+
+function preload(){
+  // load images
+  meshi = loadImage("data/face.png");
+  floor = loadImage("data/base.png");
+}
+
+function setup() {
+//Crear canvas
+    isGameOver = false;
+    createCanvas(window.innerWidth, 580);
+    //left 35% width 800px margin-left: -200px;
+    canvas.style = "position:fixed; left: 0%; width: 100%;";
+    engine = Engine.create();
+    world = engine.world;
+    Engine.run(engine);
+    reseSketch();
+    bg = loadImage("data/fondo.png");
+    var options = {
+      friction: 8,
+      restitution:0,
+      isStatic: true
+    }
+    var acciones = {
+        isStatic: true
+    }
+
+//Plataformas para saltar
+    barra1  = Bodies.rectangle(200, 450, 100, 15, options);
+    barra1.width = 100;
+    barra1.height= 15;
+    barra2 = Bodies.rectangle(300, 350, 100, 15, options);
+    barra2.width = 100;
+    barra2.height= 15;
+    barra3 = Bodies.rectangle(100, 250, 100, 15, options);
+    barra3.width = 100;
+    barra3.height= 15;
+    barra4 = Bodies.rectangle(200, 100, 100, 15, options);
+    barra4.width = 100;
+    barra4.height= 15;
+    /*barra5 = Bodies.rectangle(600, 290, 200, 15, options);
+    barra5.width = 200;
+    barra5.height= 15;*/
+    ground = Bodies.rectangle(190, height, width, 100, options);
+    ground.width = width;
+    ground.height= 100;
+    wallLeft = Bodies.rectangle(0, 0, 20, 9000 ,options);
+    wallLeft.width = 20;
+    wallLeft.height= 15000;
+    wallRight = Bodies.rectangle(width, 0, 20, 9000 ,options);
+    wallRight.width = 20;
+    wallRight.height= 15000;
+
+    //elements.push(barra5);
+    elements.push(barra2);
+    elements.push(barra3);
+    elements.push(barra4);
+    elements.push(barra1);
+    elements.push(ground);
+    elements.push(wallLeft);
+    elements.push(wallRight);
+    World.add(world, elements);
+    box = new Box(100, 509, 50, 100 );
+}
+
+
+//Crear cajitas con mouse
+function mouseDragged() {
+  boxes.push(new Box(mouseX, mouseY, 5, 5));
+}
+
+
+
+//Mover objetos con teclado
+function keyPressed(k){
+  if( k.code == "ArrowRight"){
+    var posX = box.body.position.x;
+    var posY = box.body.position.y;
+    Matter.Body.applyForce(box.body, { x: posX, y: posY }, { x: 0.05, y:0 });
+  }
+
+  if( k.code == "ArrowLeft"){
+    var posX = box.body.position.x;
+    var posY = box.body.position.y;
+    Matter.Body.applyForce(box.body, { x: posX, y: posY }, { x: -0.05, y:0 });
+  }
+
+  if( k.code == "ArrowUp"){
+    kickstart = true;
+    var posX = box.body.position.x;
+    var posY = box.body.position.y;
+    Matter.Body.applyForce(box.body, { x:posX , y:posY }, { x: 0, y: -0.1 });
+//Scroll cuando el objeto se mueve
+      //bodyScroll();
+  }
+
+  if (k.code == "R"){
+  }
+}
+
+$( document ).ready( function(){
+
+  $('#top').on( 'touchstart', function(){
+    kickstart = true;
+    var posX = box.body.position.x;
+    var posY = box.body.position.y;
+    Matter.Body.applyForce(box.body, { x:posX , y:posY }, { x: 0, y: -.1 });
+  });
+
+  $('#left').on( 'touchstart', function(){
+    var posX = box.body.position.x;
+    var posY = box.body.position.y;
+    Matter.Body.applyForce(box.body, { x: posX, y: posY }, { x: -0.05, y:0 });
+  });
+
+  $('#right').on( 'touchstart', function(){
+    var posX = box.body.position.x;
+    var posY = box.body.position.y;
+    Matter.Body.applyForce(box.body, { x: posX, y: posY }, { x: 0.05, y:0 });
+  });
+
+});//documentready
+
+//reset
+function reseSketch(){
+}
+///fin reset
+
+//Plataformas para saltar
+function draw() {
+      box.body.angle = 0;
+      background(bg);
+    //console.log( box.body.position.y );
+
+    box.show();
+    image(meshi,box.body.position.x - 60, box.body.position.y - 60, 120, 120);
+
+///Eliminar cosas del espacio
+for (var i = 0; i < boxes.length; i++) {
+    boxes[i].show();
+    if (boxes[i].offScreen()){
+
+      boxes.splice(i, 1);
+      i--;
+    }
+}
+///
+
+
+//Scroll de toda la hoja
+  screenScroll();
+    noStroke(0);
+    fill(105,105,105);
+//Plataformas para saltar
+    rectMode(CENTER);
+
+    for( var i = 0; i < elements.length; i++ ){
+      rect( elements[ i ].position.x, elements[ i ].position.y, elements[ i ].width, elements[ i ].height );
+    }
+
+    if( box.offScreen()){
+      box.removeFromWorld();
+      fill(0);
+      rectMode(CORNERS);
+      rect(0,0,window.innerWidth,window.innerHeight);
+      fill(211,211,211);
+      text("Game Over!", 22, 200);
+      textSize(60);
+      setTimeout( function(){
+        window.location.assign("index.html");
+      }, 2000);
+      console.log("game over");
+    }
+for( var i = 0; i < elements.length; i++ ){
+  rectMode( CORNERS )
+  image(floor, elements[i].position.x - 55, elements[i].position.y - 60, 110, 110);
+}
+
+}
+
+function screenScroll(){
+  if( kickstart == true ){
+  for( var i = 0; i < elements.length; i++ ){
+    Matter.Body.translate(elements[i], {x:0, y:factorIncremento});
+  }
+    scroll += factorIncremento;
+    if( scroll >= 150 ){
+      scroll = 0;
+      var options = {
+        friction: 10,
+        restitution:0,
+        isStatic: true
+      }
+      var barra = Bodies.rectangle( Math.random() * width,  0, 100, 15 ,options);
+      barra.width = 100;
+      barra.height = 15;
+      elements.push(barra);
+      World.add( world, barra );
+    }
+  }
+}
