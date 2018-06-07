229244ad03f6740e78354233f09f504e46aa441b
Jonathan mehraban <jonathanmehraban@MacBook-Pro-de-Jonathan.local>
2018-06-06 22:21:07 -0500

    audio_1

--- js/sketch.js
+++ js/sketch.js
@@ -2,12 +2,11 @@
 var Engine = Matter.Engine,
     //Render = Matter.Render,
     //Creamos el mundo
-World = Matter.World,
+    World = Matter.World,
     //Creamos los bodies ( array )
-Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies;
 
-
-    //Lista de variables
+//Lista de variables
 var engine;
 var box;
 var boxes = [];
@@ -24,26 +23,45 @@ var floor;
 var food;
 //var sky;
 var bg;
+var bgi;
+var amp;
 var sushiBar;
 var mybird;
 var score;
+var sounds = [];
+var lose;
+var soundLeft;
+var soundRight;
 
 
-
-function preload(){
-// Load PNG, GIF.
-//meshi = loadGif('data/test.gif');
-  meshi = loadAnimation("data/comp/standby1.png", "data/comp/standby17.png");
-  floor = loadImage("data/base.png");
-  leftJump  = loadAnimation("data/leftSprite/leftSprite0.png","data/leftSprite/leftSprite9.png");
-  rightJump = loadAnimation("data/rightSprite/rightSprite0.png","data/rightSprite/rightSprite9.png");
-
-
+function preload() {
+    // Load PNG, GIF.
+    //meshi = loadGif('data/test.gif');
+    meshi = loadAnimation("data/comp/standby1.png", "data/comp/standby17.png");
+    floor = loadImage("data/base.png");
+    leftJump = loadAnimation("data/leftSprite/leftSprite0.png", "data/leftSprite/leftSprite9.png");
+    rightJump = loadAnimation("data/rightSprite/rightSprite0.png", "data/rightSprite/rightSprite9.png");
+    lose = loadSound("audio/lose.wav");
+    soundLeft = loadSound("audio/left.wav");
+    soundRight = loadSound("audio/right.wav");
+    for (var i = 1; i < 8; i++) {
+        var path = "audio/t" + i + ".wav";
+        sounds.push(loadSound(path));
+    }
 }
 
 function setup() {
+  //MUSICA
+    amp = new p5.Amplitude();
+    sounds[1].loop();
+    sounds[2].loop();
+    sounds[1].setVolume(0.4);
+    sounds[2].setVolume(0.4);
 
+    sounds[1].addCue(5.3,soundPack1);
+    sounds[2].addCue(10.6,soundPack2);
 
+    //Termina MUSICA
     isGameOver = false;
     score = 0;
     createCanvas(window.innerWidth, 580);
@@ -52,58 +70,59 @@ function setup() {
     world = engine.world;
     Engine.run(engine);
     reseSketch();
+    bgi = loadImage("data/retrato.png");
+
     bg = loadImage("data/fondo.png");
-    sushiBar = loadImage("data/frontbar.png")
+
+    sushiBar = loadImage("data/frontbar.png");
     var options = {
-      friction: 8,
-      restitution:0,
-      isStatic: true
+        friction: 8,
+        restitution: 0,
+        isStatic: true
     }
     var acciones = {
         isStatic: true
     }
 
-//Plataformas para saltar
-    barra1  = Bodies.rectangle(300, 450, 100, 15, options);
+    //Plataformas para saltar
+    barra1 = Bodies.rectangle(300, 450, 100, 15, options);
     barra1.width = 100;
-    barra1.height= 15;
+    barra1.height = 15;
     barra2 = Bodies.rectangle(100, 350, 100, 15, options);
     barra2.width = 100;
-    barra2.height= 15;
+    barra2.height = 15;
     barra3 = Bodies.rectangle(300, 250, 100, 15, options);
     barra3.width = 100;
-    barra3.height= 15;
+    barra3.height = 15;
     barra4 = Bodies.rectangle(200, 100, 100, 15, options);
     barra4.width = 100;
-    barra4.height= 15;
+    barra4.height = 15;
 
     barra5 = Bodies.rectangle(0, 0, 100, 15, options);
     barra5.width = 100;
-    barra5.height= 15;
+    barra5.height = 15;
 
     barra6 = Bodies.rectangle(375, 0, 100, 15, options);
     barra6.width = 100;
-    barra6.height= 15;
+    barra6.height = 15;
 
-
-
-    /*sky = Bodies.rectangle(width/2, -200, width, 50, { isStatic: true } );
+    sky = Bodies.rectangle(width / 2, 0, width, 50, {
+        isStatic: true
+    });
     sky.width = width;
-    sky.height= 15;*/
-
+    sky.height = 15;
+    World.add(world, sky);
 
     ground = Bodies.rectangle(190, height, width, 100, options);
     ground.width = width;
-    ground.height= 100;
+    ground.height = 100;
 
-
-
-    wallLeft = Bodies.rectangle(0, 0, 20, 9000 ,options);
+    wallLeft = Bodies.rectangle(0, 0, 20, 9000, options);
     wallLeft.width = 20;
-    wallLeft.height= 15000;
-    wallRight = Bodies.rectangle(width, 0, 20, 9000 ,options);
+    wallLeft.height = 15000;
+    wallRight = Bodies.rectangle(width, 0, 20, 9000, options);
     wallRight.width = 20;
-    wallRight.height= 15000;
+    wallRight.height = 15000;
 
     elements.push(barra6);
     elements.push(barra5);
@@ -116,136 +135,193 @@ function setup() {
     elements.push(wallLeft);
     elements.push(wallRight);
     World.add(world, elements);
-    box = new Box(160, 509, 50, 100 );
+    box = new Box(160, 509, 50, 100);
 }
 
+$(document).ready(function() {
 
-$( document ).ready( function(){
+    $('#left').on('touchstart', function() {
+        kickstart = true;
+        var posX = box.body.position.x;
+        var posY = box.body.position.y;
 
+        Matter.Body.applyForce(box.body, {
+            x: posX,
+            y: posY
+        }, {
+            x: -0.05,
+            y: -0.1
+        });
+        soundLeft.play();
+        soundLeft.setVolume(0.2);
 
+    });
 
-  $('#left').on( 'touchstart', function(){
-    kickstart = true;
+    $('#right').on('touchstart', function() {
+        kickstart = true;
 
-    var posX = box.body.position.x;
-    var posY = box.body.position.y;
-    Matter.Body.applyForce(box.body, { x: posX, y: posY }, { x: -0.05, y:-0.1 });
-  });
+        var posX = box.body.position.x;
+        var posY = box.body.position.y;
+        Matter.Body.applyForce(box.body, {
+            x: posX,
+            y: posY
+        }, {
+            x: 0.05,
+            y: -0.1
+        });
+        soundRight.play();
+        soundRight.setVolume(0.15);
 
-  $('#right').on( 'touchstart', function(){
-    kickstart = true;
-    var posX = box.body.position.x;
-    var posY = box.body.position.y;
-    Matter.Body.applyForce(box.body, { x: posX, y: posY }, { x: 0.05, y:-0.1});
-  });
+    });
 
-});//documentready
+}); //documentready
 
-//reset
-function reseSketch(){
-}
+//reset NO BORRAR
+function reseSketch() {}
 ///fin reset
 
-//Plataformas para saltar
-function draw() {
-      score = score + 1;
-      box.body.angle = 0;
-      background(bg);
-    vel=box.body.velocity.x;
-  push();
-      if (vel < -.6){
-        animation(leftJump,box.body.position.x-120, box.body.position.y +20, 10, 10);
-    } else if (vel > .6){
-        animation(rightJump,box.body.position.x-30, box.body.position.y+20, 10, 10);
-    } else {
-
-        animation(meshi,box.body.position.x-4 ,box.body.position.y-45 , 10, 10);
-
-    }
-      pop();
-
-    box.show();
-
-///Eliminar cosas del espacio
-for (var i = 0; i < boxes.length; i++) {
-    boxes[i].show();
-    if (boxes[i].offScreen()){
-      boxes.splice(i, 1);
-      i--;
-    }
+function soundPack1(){
+  sounds[4].loop();
+  sounds[4].setVolume(0.4);
+  sounds[5].loop();
+  sounds[5].setVolume(0.4);
 }
-///
 
-//Scroll de toda la hoja
-  screenScroll();
-    noStroke(0);
-    fill(105,1,105);
-//Plataformas para saltar
-    rectMode(CENTER);
+function soundPack2(){
+  sounds[0].loop();
+  sounds[0].setVolume(0.4);
+  sounds[3].loop();
+  sounds[3].setVolume(0.4);
+  sounds[6].loop();
+  sounds[6].setVolume(0.4);
 
-    for( var i = 0; i < elements.length; i++ ){
-      rect( elements[ i ].position.x, elements[ i ].position.y, elements[ i ].width, elements[ i ].height );    }
+}
 
-    if( box.offScreen()){
+function death(){
+  if (box.offScreen()) {
+
       box.removeFromWorld();
+      sounds[0].stop();
+
+      sounds[4].stop();
+      sounds[5].stop();
+      sounds[6].stop();
+      lose.play();
+      lose.setVolume(0.4);
+      lose.stop();
       fill(0);
       rectMode(CORNERS);
-      rect(0,0,window.innerWidth,window.innerHeight);
-      fill(211,211,211);
+      rect(0, 0, window.innerWidth, window.innerHeight);
+      fill(211, 211, 211);
       textSize(40);
       textFont('Helvetica');
       textStyle(BOLD);
       text("Game Over!", 80, 300);
       textSize(20);
-      text(("Score: " + score ), 60, 350);
-      setTimeout( function(){
-        window.location.assign("index.html");
-      }, 1500);
+      text(("Score: " + score), 60, 350);
+      setTimeout(function() {
+          window.location.assign("index.html");
+      }, 4500);
       console.log("game over");
-    }
-for( var i = 0; i < elements.length; i++ ){
-  rectMode( CORNERS )
-  image(floor, elements[i].position.x - 55, elements[i].position.y - 60, 110, 110);
-      }
-      image(sushiBar, -12, -727);
+  }
 
-      console.log( box.body.velocity.x );
+
 }
 
+//Plataformas para saltar
+function draw() {
 
-function screenScroll(){
-  if( kickstart == true ){
-  for( var i = 0; i < elements.length; i++ ){
-    Matter.Body.translate(elements[i], {x:0, y:factorIncremento});
-  }
-    scroll += factorIncremento;
-    if( scroll >= 150 ){
-      scroll = 0;
-      var options = {
-        friction: 10,
-        restitution:0,
-        isStatic: true
-      }
-      var cajita = (Math.random() * width);
+    score = score + 1;
+    box.body.angle = 0;
+    background(bg);
 
-      var barra = Bodies.rectangle( cajita,  0, 100, 15 ,options);
-      barra.width = 100;
-      barra.height = 15;
-      elements.push(barra);
-  World.add( world, barra );
-//comida
+//MUSICA e imagen
+    push();
+    var vol = amp.getLevel();
+    var diam = map(vol, 0, 0.4, 1, 100);
+    image(bgi, 0, 0 - diam, bgi.width / 2, bgi.height / 2 - diam );
+    pop();
+// termina MUSICA
+    vel = box.body.velocity.x;
+    push();
+    if (vel < -.6) {
+        animation(leftJump, box.body.position.x - 120, box.body.position.y + 20, 10, 10);
+    } else if (vel > .6) {
+        animation(rightJump, box.body.position.x - 30, box.body.position.y + 20, 10, 10);
+    } else {
 
-/*      var barra = Bodies.rectangle( cajita,  -30, 100, 15 ,options);
-      barra.width = 50;
-      barra.height = 50;
-
-      elements.push(barra);
-
-      World.add( world, barra );
-*/
-
-
+        animation(meshi, box.body.position.x - 4, box.body.position.y - 45, 10, 10);
 
     }
-  }
+    pop();
+
+    box.show();
+
+    ///Eliminar cosas del espacio
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].show();
+        if (boxes[i].offScreen()) {
+            boxes.splice(i, 1);
+            i--;
+        }
+    }
+    //Scroll de toda la hoja
+    screenScroll();
+    noStroke(0);
+    fill(105, 1, 105);
+    //Plataformas para saltar
+    rectMode(CENTER);
+    fill(255);
+    for (var i = 0; i < elements.length; i++) {
+        rect(elements[i].position.x, elements[i].position.y, elements[i].width, elements[i].height);
+    }
+
+
+    for (var i = 0; i < elements.length; i++) {
+        rectMode(CORNERS);
+        image(floor, elements[i].position.x - 55, elements[i].position.y - 60, 110, 110);
+    }
+    image(sushiBar, -12, -727);
+
+    //console.log( box.body.velocity.x );
+}
+
+function screenScroll() {
+    //console.log( scroll );
+    //sky.position.y = scroll;
+    if (kickstart == true) {
+        for (var i = 0; i < elements.length; i++) {
+            Matter.Body.translate(elements[i], {
+                x: 0,
+                y: factorIncremento
+            });
+        }
+        scroll += factorIncremento;
+        //
+        if (scroll >= 150) {
+            scroll = 0;
+            var options = {
+                friction: 10,
+                restitution: 0,
+                isStatic: true
+            }
+            var cajita = (Math.random() * width);
+
+            var barra = Bodies.rectangle(cajita, 0, 100, 15, options);
+            barra.width = 100;
+            barra.height = 15;
+            elements.push(barra);
+            World.add(world, barra);
+            //comida
+
+            var barra = Bodies.rectangle(cajita, -30, 100, 15, options);
+            barra.width = 50;
+            barra.height = 50;
+
+            elements.push(barra);
+
+            World.add(world, barra);
+        }
+         death();
+    }
 }
